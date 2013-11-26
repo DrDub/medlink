721573309820cebc63cea5ea95920d130086f589
Al Snow <jasnow@hotmail.com>
2013-11-26 11:02:33 -0500

    Second try at clickatell #164

---
+++ spec/controllers/clickatell_controller_spec.rb
@@ -0,0 +1,89 @@
+require 'spec_helper'
+
+describe ClickatellController do
+  include SmsSpec::Helpers
+
+  let(:number) { '+15555555555' }
+  let(:current_user) { FactoryGirl.create(:user, pcv_id: '123456') }
+
+  before(:each) do
+    FactoryGirl.create :supply, shortcode: 'ASDF'
+    sign_in current_user
+  end
+
+  describe "POST 'receive'" do
+
+    it 'routes lists' 
+# @wip
+#do
+#      expect{ post :receive, From: number, Body: 'List supplies'
+#        }.to raise_error /Not Implemented/
+#    end
+
+    # -- i18n codes -----
+    {
+      unparseable:            'This message should not parse as a valid order',
+      confirmation:           '123456, ASDF, 30mg, 50, Somewhere',
+      unrecognized_pcvid:     'XXX,    ASDF, 30mg, 50, Somewhere',
+      unrecognized_shortcode: '123456, XXX,  30mg, 50, Somewhere'
+    }.each do |key, msg|
+      it "sends order.#{key} when appropriate" 
+# @wip
+#do
+#        post :receive, From: number, Body: msg
+#        open_last_text_message_for number
+#        current_text_message.should have_body I18n.t "order.#{key}"
+#      end
+    end
+
+    # -- English translations -----
+    # Note that we have some translations for events that can't ever happen
+    #   (like dose being invalid). Such is the nature of spec-driven
+    #   development.
+    #
+    # TODO: are these deliberately inconsistent in phrasing "the/your request"?
+    # ANSWER: Check Design document for phrasing.
+    #
+    # From Design Doc: SE1(unrecognized_pcvid), SE2(unrecognized_shortcode),
+    #    SE3(invalid_dose), SE4(invalid_quantity), SE5(invalid_quantity)
+    { unrecognized_pcvid: "PCVID Invalid: Your request was " +
+        "not submitted because the PCVID was incorrect. Please resubmit " +
+        "your request in this format: PCVID, Supply short name, dose, " +
+        "qty, location.",
+      unrecognized_shortcode: "Supply short name invalid: " +
+        "Your request was not submitted because supply name was incorrect. " +
+        "Please resubmit the request in this format: PCVID, Supply " +
+        "short name, dose, qty, location.",
+      invalid_dose: "Dose invalid: " +
+        "Your request was not submitted because dose was incorrect. " +
+        "Please resubmit the request in this format: PCVID, Supply " +
+        "short name, dose, qty, location.",
+      invalid_quantity: "Qty invalid: " +
+        "Your request was not submitted because quantity was incorrect. " +
+        "Please resubmit the request in this format: PCVID, Supply " +
+        "short name, dose, qty, location.",
+      invalid_location: "Location invalid: " +
+        "Your request was not submitted because location was incorrect. " +
+        "Please resubmit the request in this format: PCVID, Supply " +
+        "short name, dose, qty, location."
+    }.each do |key, translation|
+      it "translates order.#{key} correctly into English" 
+# @wip
+#do
+#        expect( I18n.t "order.#{key}" ).to eq translation
+#      end
+    end
+
+    it 'notifies on duplicate submission' 
+# @wip
+#do
+#      msg = '123456, ASDF, 30mg, 50, Somewhere'
+#      3.times do
+#        post :receive, From: number, Body: msg
+#        open_last_text_message_for number
+#      end
+# @wip      current_text_message.should have_body I18n.t "order.duplicate_order"
+#    end
+
+  end
+end
